9fe6222d1489c4a6e5d3137ecd2df89305e10955
partho1999 <sarker624452@gmail.com>
2023-03-14 12:13:08 +0600

    version 1.1.2

--- all_products_scraper.py
+++ all_products_scraper.py
@@ -7,16 +7,22 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 import time
+from selenium.webdriver.common import keys
 
 
 def scraper(url):
-    driver = webdriver.Chrome(executable_path="D:\Desktop\japanices-addius-website-scrape\chromedriver.exe")
+    driver = webdriver.Chrome(executable_path="E:\Scraper\chromedriver.exe")
 
 
     driver.get(url)
 
     get_url = driver.current_url
     print("The current url is:"+str(get_url))
+
+    try:
+        button = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH,'//*[@id="__next"]/div/div[1]/div[4]/main/div/div/div[2]/div[1]/div[2]/div[3]/div/div/div[1]/div/div[1]/ul/li/div'))).send_keys(keys.Keys.ENTER)
+    except:
+        print('not clicked')
 
     for j in range(0,10):
         driver.execute_script("scrollBy(0,+500);")
@@ -25,11 +31,18 @@ def scraper(url):
         time.sleep(3)
         print("scrolling..")
 
+    
+
     soup=BeautifulSoup(driver.page_source,"html.parser")
     print(soup.title.text)
 
     for img in soup.find_all('table'):
         print(img.get('class'))
+
+    file_name = get_url.replace("https://shop.adidas.jp/","")
+
+    file_name = file_name.split('/')[-2]
+    print(file_name)
 
     #img URL##############################################################
     images = soup.find_all('img')
@@ -45,8 +58,8 @@ def scraper(url):
     print(image)
 
     df1 = pd.DataFrame(image,  columns =['img_url'])
-    timestr = time.strftime("%Y%m%d-%H%M%S")
-    df1.to_csv("img-url/img-url"+"-"+timestr+".csv")
+    # timestr = time.strftime("%Y%m%d-%H%M%S")
+    # df1.to_csv("img-url/img-url"+"-"+timestr+".csv")
     print(df1)
 
     #Breadcrumb(Category)###################################################
@@ -59,8 +72,8 @@ def scraper(url):
     # for li in ul.findAll('li'):
     #     print(li)
     df2 = pd.DataFrame(b_cat_ul,  columns =['Breadcrumb(Category)'])
-    timestr = time.strftime("%Y%m%d-%H%M%S")
-    df2.to_csv('Breadcrumb(Category)/Breadcrumb(Category)'+'-'+timestr+'.csv')
+    # timestr = time.strftime("%Y%m%d-%H%M%S")
+    # df2.to_csv('Breadcrumb(Category)/Breadcrumb(Category)'+'-'+timestr+'.csv')
     print(df2)
 
     #Category###############################################################
@@ -86,8 +99,8 @@ def scraper(url):
     dict = {'Category': cat_lst, 'Product Name': pro_t_lst, 'Product Price': pro_price_lst} 
         
     df3 = pd.DataFrame(dict)
-    timestr = time.strftime("%Y%m%d-%H%M%S")    
-    df3.to_csv("product_details/product_details"+"-"+timestr+".csv")
+    # timestr = time.strftime("%Y%m%d-%H%M%S")    
+    # df3.to_csv("product_details/product_details"+"-"+timestr+".csv")
     print(df3)
 
     #Available Size#############################################################
@@ -102,28 +115,35 @@ def scraper(url):
             a_size_lst.append(ul.text)
 
         df4 = pd.DataFrame(a_size_lst,  columns =['Available Size'])
-        timestr = time.strftime("%Y%m%d-%H%M%S")
-        df4.to_csv('Available-Size/Available-Size'+'-'+timestr+'.csv')
+        # timestr = time.strftime("%Y%m%d-%H%M%S")
+        # df4.to_csv('Available-Size/Available-Size'+'-'+timestr+'.csv')
         print(df4)
 
     #Sense of the Size###########################################################
     sense_size = soup.find("div", {"class": "sizeFitBar css-zrdet1"})
     if sense_size is None:
-        pass
+        #s_size_lst=[]
+        df5=pd.DataFrame()
+        print(df5)
     else:
         s_size_lst=[]
         for t in sense_size.find_all("span"):
             s_size_lst.append(t.text)
 
         df5 = pd.DataFrame(s_size_lst,  columns =['Sense of the Size'])
-        timestr = time.strftime("%Y%m%d-%H%M%S")
-        df5.to_csv('Sense of the Size/Sense of the Size'+'-'+timestr+'.csv')
+        # timestr = time.strftime("%Y%m%d-%H%M%S")
+        # df5.to_csv('Sense of the Size/Sense of the Size'+'-'+timestr+'.csv')
         print(df5)
 
     #Coordinate Product Name#####################################################
     coordinate_title = soup.find("span", {"class": "titleWrapper"})
     if coordinate_title is None:
-        pass
+        # coor_n_lst=[]
+        # coor_p_lst=[]
+        # pg_lst =[]
+        # img_lst=[]
+        df6=pd.DataFrame()
+        print(df6)
     else:
 
         coor_n_lst=[coordinate_title.text]
@@ -152,55 +172,59 @@ def scraper(url):
 
         print(img_lst)
 
-        dic ={'Product Name':coor_n_lst, 'Product Price':coor_p_lst, 'Product Details Page':pg_lst, 'Product Img URL':img_lst}
+        dic1 ={'coordinator Product Name':coor_n_lst, 'coordinator Product Price':coor_p_lst, 'coordinator Product Details Page':pg_lst, 'coordinator Product Img URL':img_lst}
 
-        df5 = pd.DataFrame(dic)
-        timestr = time.strftime("%Y%m%d-%H%M%S")
-        df5.to_csv('coordinate product details/coordinate product details'+'-'+timestr+'.csv')
-        print(df5)
+        df6 = pd.DataFrame(dic1)
+        # timestr = time.strftime("%Y%m%d-%H%M%S")
+        # df5.to_csv('coordinate product details/coordinate product details'+'-'+timestr+'.csv')
+        print(df6)
 
     # Title of Description########################################################
     title_des = soup.find("h4", {"class": "heading itemFeature test-commentItem-subheading"})
     if title_des is None:
-        pass
+        t_o_d_lst=[]
+
     else:
         
         t_o_d_lst=[title_des.text]
         #print(t_o_d_lst)
-        df6 = pd.DataFrame(t_o_d_lst,  columns =['Title of Description'])
-        timestr = time.strftime("%Y%m%d-%H%M%S")
-        df6.to_csv('Title of Description/Title of Description'+'-'+timestr+'.csv')
-        print(df6)
+    df7 = pd.DataFrame(t_o_d_lst,  columns =['Title of Description'])
+    # timestr = time.strftime("%Y%m%d-%H%M%S")
+    # df6.to_csv('Title of Description/Title of Description'+'-'+timestr+'.csv')
+    print(df7)
 
     # General Desciption of the product#############################################
     des = soup.find("div", {"class": "commentItem-mainText test-commentItem-mainText"})
     if des is None:
-        pass
+        g_d_o_t_p_lst=[]
     else:
         g_d_o_t_p_lst=[des.text]
-        df7 = pd.DataFrame(g_d_o_t_p_lst,  columns =['General Desciption of the product'])
-        timestr = time.strftime("%Y%m%d-%H%M%S")
-        df7.to_csv('General Desciption of the product/General Desciption of the product'+'-'+timestr+'.csv')
-        print(df7)
+    df8 = pd.DataFrame(g_d_o_t_p_lst,  columns =['General Desciption of the product'])
+    #timestr = time.strftime("%Y%m%d-%H%M%S")
+    #df7.to_csv('General Desciption of the product/General Desciption of the product'+'-'+timestr+'.csv')
+    print(df8)
 
     # General Discription (itemization)##############################################
     des_ul = soup.find("ul", {"class": "articleFeatures description_part css-1lxspbu"})
     if des_ul is None:
-        pass
+        g_d_lst=[]
     else:
         g_d_lst=[]
         for ul in des_ul:
             g_d_lst.append(ul.text)
 
-        df8 = pd.DataFrame(g_d_lst,  columns =['General Discription (itemization)'])
-        timestr = time.strftime("%Y%m%d-%H%M%S")
-        df8.to_csv('General Discription (itemization)/General Discription (itemization)'+'-'+timestr+'.csv')
-        print(df8)
+    df9 = pd.DataFrame(g_d_lst,  columns =['General Discription (itemization)'])
+    #timestr = time.strftime("%Y%m%d-%H%M%S")
+    #df8.to_csv('General Discription (itemization)/General Discription (itemization)'+'-'+timestr+'.csv')
+    print(df9)
 
     #Tale of Size###################################################################
     sizetable =  soup.find('table')
     if sizetable is None:
-        pass
+        
+        df10 = pd.DataFrame()
+        print(df10)
+        
     else:
         sizetable0 =  soup.findAll('table', class_='sizeChartTable')[0]
         
@@ -221,13 +245,14 @@ def scraper(url):
                 rows[indexvalues[i]]=row_data
             i+=1
 
-        df9 = pd.DataFrame.from_dict(rows, orient='index')
-        df9.columns=df9. iloc[0]
-        df9 = df9.iloc[1:]
-        timestr = time.strftime("%Y%m%d-%H%M%S")
-        df9.to_csv('Tale-of-Size/Tale-of-Size'+'-'+timestr+'.csv')
-        print(df9)
+        df10 = pd.DataFrame.from_dict(rows, orient='index')
+        df10.columns=df10. iloc[0]
+        df10 = df10.iloc[1:]
+        #timestr = time.strftime("%Y%m%d-%H%M%S")
+        #df9.to_csv('Tale-of-Size/Tale-of-Size'+'-'+timestr+'.csv')
+        print(df10)
 
+    #Rating, Number of Reviews, Recommended Rate,####################################
     rating = soup.find("span", {"class": "BVRRNumber BVRRRatingNumber"})
     if rating is None:
         rating_lst=[]
@@ -253,14 +278,29 @@ def scraper(url):
         print(recomadation_lst)
 
     dict = {'Rating': rating_lst, 'Number of Reviews': review_lst, 'Recommended rate': recomadation_lst}   
-    df10 = pd.DataFrame(dict)
-    timestr = time.strftime("%Y%m%d-%H%M%S")
-    df10.to_csv('Rating-Review-Recommended/Rating-Review-Recommended'+'-'+timestr+'.csv')
+    df11 = pd.DataFrame(dict)
+    #timestr = time.strftime("%Y%m%d-%H%M%S")
+    #df10.to_csv('Rating-Review-Recommended/Rating-Review-Recommended'+'-'+timestr+'.csv')
+    print(df11)
 
+    #Each Single information of User Review Such as#######################################
+    
     RatingsContainer = soup.find_all("div", {"class": "BVRRSecondaryRatingsContainer"})
     print(RatingsContainer)
     if RatingsContainer is None:
-        pass
+        # rf_list=[]
+        # rf_s_lst=[]
+        # rl_list=[]
+        # rl_s_lst=[]
+        # rq_list=[]
+        # rq_s_lst=[]
+        # rc_list=[]
+        # rc_s_lst=[]
+        df12=pd.DataFrame()
+        df13=pd.DataFrame()
+        print(df12)
+        print(df13)
+
     else:
 
         #RatingFit
@@ -339,33 +379,41 @@ def scraper(url):
             rc_s_lst =[rc_score]
             print(rc_s_lst)
 
-        dic={
+        dic2={
             'Sense of Fitting': rf_list,
             'Appropriation of length': rl_list,
             'Quality of material' : rq_list,
             'Comfort': rc_list   
         }
 
-        df11 = pd.DataFrame(dic)
-        timestr = time.strftime("%Y%m%d-%H%M%S")
-        df11.to_csv('Sense-of-Fitting-Appropriation-of-length-Quality-of-material-Comfort/Sense-of-Fitting-Appropriation-of-length-Quality-of-material-Comfort'+'-'+timestr+'.csv')
-        print(df11)
+        df12 = pd.DataFrame(dic2)
+        #timestr = time.strftime("%Y%m%d-%H%M%S")
+        #df12.to_csv('Sense-of-Fitting-Appropriation-of-length-Quality-of-material-Comfort/Sense-of-Fitting-Appropriation-of-length-Quality-of-material-Comfort'+'-'+timestr+'.csv')
+        print(df12)
 
-        dic={
+        dic3={
             'Fitting Rating': rf_s_lst,
             'length Rating': rl_s_lst,
             'material Rating' : rq_s_lst,
             'Comfort Rating': rc_s_lst  
         }
 
-        df_r= pd.DataFrame(dic)
-        timestr = time.strftime("%Y%m%d-%H%M%S")
-        df_r.to_csv('Fitting-length-Quality-Comfort-Rating/Fitting-length-Quality-Comfort-Rating'+'-'+timestr+'.csv')
+        df13= pd.DataFrame(dic3)
+        #timestr = time.strftime("%Y%m%d-%H%M%S")
+        #df_12.to_csv('Fitting-length-Quality-Comfort-Rating/Fitting-length-Quality-Comfort-Rating'+'-'+timestr+'.csv')
+        print(df13)
 
+    # Each single information of User Review such as Date, Rating, Review title, Review Description,Reviewer ID######
+         
     user_review_cnt = soup.find('div', class_='BVRRContentReview BVRRDisplayContentReview BVDIContentNative BVRRContentReviewNative BVContentJaJp BVDI_BAContentVerifiedPurchaser BVRRDisplayContentReviewOdd BVRRDisplayContentReviewFirst BVRROdd BVRRFirst')
     #print(user_review_cnt)
     if user_review_cnt is None:
-        pass
+        # r_d_lst=[]
+        # r_r_lst=[]
+        # r_t_lst=[]
+        # r_d_lst=[]
+        df14=pd.DataFrame()
+        print(df14)
     else:
 
     
@@ -388,17 +436,17 @@ def scraper(url):
         r_d_lst=[user_review_des.text]
         print(r_d_lst)
 
-        dic={
+        dic4={
             'Date':r_d_lst, 
             'Rating':r_r_lst,
             'Review title':r_t_lst,
             'Review descriptions':r_d_lst
         }
 
-        df12 = pd.DataFrame(dic)
-        timestr = time.strftime("%Y%m%d-%H%M%S")
-        df12.to_csv('Verified-Product-Reviewer-Details/Verified-Product-Reviewer-Details'+'-'+timestr+'.csv')
-        print(df12)
+        df14 = pd.DataFrame(dic4)
+        #timestr = time.strftime("%Y%m%d-%H%M%S")
+        #df14.to_csv('Verified-Product-Reviewer-Details/Verified-Product-Reviewer-Details'+'-'+timestr+'.csv')
+        print(df14)
 
     review_date_lst =[]
     review_rating_lst=[]
@@ -474,7 +522,7 @@ def scraper(url):
             review_id_lst.append(odd_review_id.text)
             print(review_id_lst)
 
-    dic={
+    dic5={
         'Date':review_date_lst, 
         'Rating':review_rating_lst,
         'Review title':review_title_lst,
@@ -482,11 +530,12 @@ def scraper(url):
         'Reviewer ID':review_id_lst
     }
 
-    df13 = pd.DataFrame(dic)
-    timestr = time.strftime("%Y%m%d-%H%M%S")
-    df13.to_csv('product-Reviewer-details/product Reviewer details'+'-'+timestr+'.csv')
-    print(df13)
-
+    df15 = pd.DataFrame(dic5)
+    #timestr = time.strftime("%Y%m%d-%H%M%S")
+    #df15.to_csv('product-Reviewer-details/product Reviewer details'+'-'+timestr+'.csv')
+    print(df15)
+    
+    #KW#################################################################################
     kw = soup.find("div", {"class": "test-category_link null css-vxqsdw"})
     #print(kw)
     kw_lst=[]
@@ -496,18 +545,40 @@ def scraper(url):
     #print(kw_lst)
 
 
-    df14 = pd.DataFrame([[str(str(kw_lst)[1:-1])]], columns =['KW'])
-    timestr = time.strftime("%Y%m%d-%H%M%S")
-    df14.to_csv('KW/KW'+'-'+timestr+'.csv')
-    print(df14)
+    df16 = pd.DataFrame([[str(str(kw_lst)[1:-1])]], columns =['KW'])
+    # timestr = time.strftime("%Y%m%d-%H%M%S")
+    # df16.to_csv('KW/KW'+'-'+timestr+'.csv')
+    print(df16)
+
+    with pd.ExcelWriter(file_name+'.xlsx', engine='xlsxwriter') as writer:
+        df1.to_excel(writer, sheet_name=file_name, startrow=1, startcol=0)
+        df2.to_excel(writer, sheet_name=file_name, startrow=1+len(df1)+3, startcol=0)
+        df3.to_excel(writer, sheet_name=file_name, startrow=1+len(df1)+3+1+len(df2)+3, startcol=0)
+        df4.to_excel(writer, sheet_name=file_name, startrow=1+len(df1)+3+1+len(df2)+3+1+len(df3)+3, startcol=0)
+        df5.to_excel(writer, sheet_name=file_name, startrow=1+len(df1)+3+1+len(df2)+3+1+len(df3)+3+1+len(df4)+3, startcol=0)
+        df6.to_excel(writer, sheet_name=file_name, startrow=1+len(df1)+3+1+len(df2)+3+1+len(df3)+3+1+len(df4)+3+1+len(df5)+3, startcol=0)
+        df7.to_excel(writer, sheet_name=file_name, startrow=1+len(df1)+3+1+len(df2)+3+1+len(df3)+3+1+len(df4)+3+1+len(df5)+3+1+len(df6)+3, startcol=0)
+        df8.to_excel(writer, sheet_name=file_name, startrow=1+len(df1)+3+1+len(df2)+3+1+len(df3)+3+1+len(df4)+3+1+len(df5)+3+1+len(df6)+3+1+len(df7)+3, startcol=0)
+        df9.to_excel(writer, sheet_name=file_name, startrow=1+len(df1)+3+1+len(df2)+3+1+len(df3)+3+1+len(df4)+3+1+len(df5)+3+1+len(df6)+3+1+len(df7)+3+1+len(df8)+3, startcol=0)
+        df10.to_excel(writer, sheet_name=file_name, startrow=1+len(df1)+3+1+len(df2)+3+1+len(df3)+3+1+len(df4)+3+1+len(df5)+3+1+len(df6)+3+1+len(df7)+3+1+len(df8)+3+1+len(df9)+3, startcol=0)
+        df11.to_excel(writer, sheet_name=file_name, startrow=1+len(df1)+3+1+len(df2)+3+1+len(df3)+3+1+len(df4)+3+1+len(df5)+3+1+len(df6)+3+1+len(df7)+3+1+len(df8)+3+1+len(df9)+3+1+len(df10)+3, startcol=0)
+        df12.to_excel(writer, sheet_name=file_name, startrow=1+len(df1)+3+1+len(df2)+3+1+len(df3)+3+1+len(df4)+3+1+len(df5)+3+1+len(df6)+3+1+len(df7)+3+1+len(df8)+3+1+len(df9)+3+1+len(df10)+3+1+len(df11)+3, startcol=0)
+        df13.to_excel(writer, sheet_name=file_name, startrow=1+len(df1)+3+1+len(df2)+3+1+len(df3)+3+1+len(df4)+3+1+len(df5)+3+1+len(df6)+3+1+len(df7)+3+1+len(df8)+3+1+len(df9)+3+1+len(df10)+3+1+len(df11)+3+1+len(df12)+3, startcol=0)
+        df14.to_excel(writer, sheet_name=file_name, startrow=1+len(df1)+3+1+len(df2)+3+1+len(df3)+3+1+len(df4)+3+1+len(df5)+3+1+len(df6)+3+1+len(df7)+3+1+len(df8)+3+1+len(df9)+3+1+len(df10)+3+1+len(df11)+3+1+len(df12)+3+1+len(df13)+3, startcol=0)
+        df15.to_excel(writer, sheet_name=file_name, startrow=1+len(df1)+3+1+len(df2)+3+1+len(df3)+3+1+len(df4)+3+1+len(df5)+3+1+len(df6)+3+1+len(df7)+3+1+len(df8)+3+1+len(df9)+3+1+len(df10)+3+1+len(df11)+3+1+len(df12)+3+1+len(df13)+3+1+len(df14)+3, startcol=0)
+        df16.to_excel(writer, sheet_name=file_name, startrow=1+len(df1)+3+1+len(df2)+3+1+len(df3)+3+1+len(df4)+3+1+len(df5)+3+1+len(df6)+3+1+len(df7)+3+1+len(df8)+3+1+len(df9)+3+1+len(df10)+3+1+len(df11)+3+1+len(df12)+3+1+len(df13)+3+1+len(df14)+3+1+len(df15)+3, startcol=0)
+        #df17.to_excel(writer, sheet_name=file_name, startrow=1+len(df1)+3+1+len(df2)+3+1+len(df3)+3+1+len(df4)+3+1+len(df5)+3+1+len(df6)+3+1+len(df7)+3+1+len(df8)+3+1+len(df9)+3+1+len(df10)+3+1+len(df11)+3+1+len(df12)+3+1+len(df13)+3+1+len(df14)+3+1+len(df15)+3+1+len(df16)+3, startcol=0)
 
 
 
-def get_url(category):
+
+
+
+def get_url(category,page):
     # Create webdriver object
     driver = webdriver.Chrome(executable_path="E:\Scraper\chromedriver.exe")
 
-    driver.get('https://shop.adidas.jp/item/?category='+category+'&gender=mens&limit=48')
+    driver.get('https://shop.adidas.jp/item/?category='+category+'&gender=mens&limit=48&page='+page)
     # r = requests.get(url)
     # #print(r.text)
 
@@ -540,11 +611,13 @@ def get_url(category):
 
     df = pd.DataFrame(product_url,  columns =['product_details_url'])
     df = df.drop(df[df['product_details_url']=="https://shop.adidas.jp/adiclub/membersweek/"].index)
+    df = df.drop(df[df['product_details_url']=="https://shop.adidas.jp/item/?collection=tiro_avryn"].index)
     df = df.drop_duplicates()
 
 
-    timestr = time.strftime("%Y%m%d-%H%M%S")
-    df.to_csv("product-details-url-list/product-details-url-list"+"-"+timestr+".csv")
+    #timestr = time.strftime("%Y%m%d-%H%M%S")
+    #df.to_csv("product-details-url-list"+"-"+timestr+".csv")
+    df.to_csv('url_list.csv', mode='a', header=False)
     print(df)
 
     col_list = df.product_details_url.values.tolist()
@@ -552,20 +625,21 @@ def get_url(category):
         scraper(url)
         time.sleep(10)
 
-#get_url('accessories')
-
-# for i in range(1,4):# set range how much page you want to scrape 
+# for i in range(2,4):# set range how much page you want to scrape 
 #     print(i)
-#     get_url('wear',str(i)) #set the category which you want to scrape
+#     get_url('accessories',str(i)) #set the category which you want to scrape
+
+#https://shop.adidas.jp/products/GF0210/
 
 def test():
-    df = pd.read_csv(r'D:\Desktop\japanices-addius-website-scrape\product-details-url-list\product-details-url-list-20230311-215827.csv')
+    df = pd.read_csv(r'E:\assienment of backend Engineer(Python)\url_list.csv')
 
     #print(df.head(96))
     col_list = df.product_details_url.values.tolist()
-    #print(len(col_list))
-    #print(col_list.index('https://shop.adidas.jp/products/HF7045/'))
-    filter_col = col_list[43:]
+    #print(col_list)
+    #print(col_list.index('https://shop.adidas.jp/products/HA5951/'))
+    filter_col = col_list[25:]
+    #print(filter_col)
 
     for url in filter_col:
         scraper(url)
